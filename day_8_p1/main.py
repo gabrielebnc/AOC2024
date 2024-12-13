4ef5ff087e71e5d5f50d57f215cceb16d5c89738
Gabboooo <gabriele.bianchi21@gmail.com>
2024-12-13 14:51:07 +0100

    day 8 part 1

---
+++ day_8_p1/main.py
@@ -0,0 +1,60 @@
+def is_inside_map(pos: tuple[int, int], bounds: tuple[int, int]):
+    return (pos[0] >= 0 and pos[0] < bounds[0]) and (pos[1] >= 0 and pos[1] < bounds[1])
+
+
+def add_positions(pos1: tuple[int, int], pos2: tuple[int, int]):
+    return tuple(a + b for a, b in zip(pos1, pos2))
+
+
+def subtract_positions(pos1: tuple[int, int], pos2: tuple[int, int]):
+    return tuple(a - b for a, b in zip(pos1, pos2))
+
+
+def get_antinodes_positions(pos1: tuple[int, int], pos2: tuple[int, int], bounds):
+    def validate_antinode_pos(pos: tuple[int, int]):
+        return pos != pos1 and pos != pos2 and is_inside_map(pos, bounds)
+
+    an_positions = []
+    delta_pos = (pos1[0] - pos2[0], pos1[1] - pos2[1])
+
+    #Compute all the possible antinodes positions, two of these will be the antennas positions themself, we will filter them out
+    an_positions.append(add_positions(pos1, delta_pos))
+    an_positions.append(add_positions(pos2, delta_pos))
+    an_positions.append(subtract_positions(pos1, delta_pos))
+    an_positions.append(subtract_positions(pos1, delta_pos))
+    # Now filter, removing the positions identical to pos1 or pos2 and positions outside the map
+    return list(set(filter(validate_antinode_pos, an_positions)))
+
+
+def main():
+    input_map = []
+    antennas_frequencies = set()
+    antinode_positions = set()
+
+    with open("input.txt", "r") as input:
+        while line := list(input.readline().strip()):
+            input_map.append(line)
+            for e in line:
+                antennas_frequencies.add(e)
+    antennas_frequencies.remove(".")
+
+    bounds = (len(input_map), len(input_map[0]))
+
+    for x1, line1 in enumerate(input_map):
+        for y1, elem1 in enumerate(line1):
+            if (
+                elem1 in antennas_frequencies
+            ):  # Skip for each spot which is not an antenna
+                for x2, line2 in enumerate(input_map):
+                    for y2, elem2 in enumerate(line2):
+                        if elem1 == elem2:  # We found two antennas of same frequency
+                            antinodes = get_antinodes_positions((x1, y1), (x2, y2), bounds)
+                            for an in antinodes:
+                                antinode_positions.add(an)
+
+    return len(antinode_positions)
+
+
+if __name__ == "__main__":
+    res = main()
+    print(f"Result: {res}")
